e48de412bc6e6fdcf1c2328cccf1b3d25e5b2dd5
Jana Alhumaidan <122381274+JanaAlhumaidan@users.noreply.github.com>
2024-04-28 01:39:46 +0300

    done w all fun

--- CS221-project1-structure-case4-5-7.cpp
+++ CS221-project1-structure-case4-5-7.cpp
@@ -8,11 +8,13 @@ using namespace std;
 const int rowMax = 50;
 const int cols = 4;
 int rowCount = 0;
+int i;
 
 int displayCount = 0;
 int searchCount = 0;
 int Addcount = 0;
 int DeleteCount = 0;
+
 struct info {
     string id;
     string name;
@@ -27,47 +29,58 @@ void Add();
 void Delete();
 void Display();
 int search(const string& chosenId);
+void sortById();
+void saveToFile();
+void updateScore(string idToUpdate, string newScore);
 
 int main() {
     readDataFromFile("gameData.txt");
     cout << "ID GamerName GameName Score" << endl;
     Display();
 
+    string newScore, idToUpdate;
     string chosenId;
     do {
         switch (menu()) {
-        case 1:
-            Add();
-            break;
-        case 2:
-            Delete();
-            break;
-    
-        /*case 3:
-        // Update score functionality   --out
-        break;*/
-        case 4:
-        Display();
-        break;
-        /*case 5:
-            cout << "Enter the ID: ";
-            cin >> chosenId;
-            int i = search(chosenId);
-            if (i != -1) {
-                cout << data[i].id << " " << data[i].name << " " << data[i].game << " " << data[i].score << endl;
-            }
-            else {
-                cout << "ID not found" << endl;
-            }
-            break;*/
-        /*case 6:
-            // Sort gamers by ID functionality  --out
-            break;
-        case 7:
-            cout << "Exiting program..." << endl;      // --out
-            return 0;
-        default:
-            cout << "\n Incorrect menu option. Please try another option." << endl;*/
+            case 1:
+                Add();
+                break;
+            case 2:
+                Delete();
+                break;
+            case 3:
+                cout << "id to update: " ;
+                cin >> idToUpdate;
+                cout << "new score: ";
+                cin >> newScore;
+                updateScore(idToUpdate, newScore);
+                break;
+            
+            case 4:
+                Display();
+                break;
+            case 5:
+                cout << "Enter the ID: ";
+                cin >> chosenId;
+                i = search(chosenId);
+                if (i != -1) {
+                    cout << data[i].id << " " << data[i].name << " " << data[i].game << " " << data[i].score << endl;
+                }
+                else {
+                    cout << "ID not found" << endl;
+                }
+                break;
+            case 6:
+                sortById();
+                break;
+            case 7:
+                saveToFile();
+                cout << displayCount;
+                cout << "Exiting program..." << endl;  
+                return 0;
+            default:
+                cout << "\n Incorrect menu option. Please try another option." << endl;
+                break;
         }
     } while (true);
     return 0;
@@ -89,6 +102,7 @@ int menu() {
 }
 
 void readDataFromFile(const string& filename) {
+    rowCount = 0;
     ifstream inFile(filename);
     if (!inFile.is_open()) {
         cout << "Error opening file: " << filename << endl;
@@ -123,16 +137,18 @@ void Add() {
     if (!outFile.is_open()) {
         cout << "Error opening file for writing." << endl;
     }
-    outFile << newId << endl << newName << endl << newGame << endl << newScore << endl;
+    outFile 
+        << newId << endl 
+        << newName << endl 
+        << newGame << endl 
+        << newScore << endl;
 
     cout << "New gamer added successfully!" << endl;
     Addcount ++;
-    // Close the output file
     outFile.close();
 }
 
 void Delete() {
-    // Delete gamer functionality  --out
     string x;
     cout << "Enter the ID of the gamer to delete: ";
     cin >> x;
@@ -149,9 +165,13 @@ void Delete() {
         cout << "Error creating temporary file." << endl;
     }
 
-    for (int i = 0; i < rowCount; ++i) {
+    for ( i = 0; i < rowCount; ++i) {
         if (i != foundIndex) {
-            tempFile << data[i].id << endl << data[i].name << endl << data[i].game << endl << data[i].score << endl;
+            tempFile 
+                << data[i].id << endl 
+                << data[i].name << endl 
+                << data[i].game << endl 
+                << data[i].score << endl;
         }
     }
     tempFile.close();
@@ -163,22 +183,71 @@ void Delete() {
 }
 
 void Display() {
+    readDataFromFile("gameData.txt");
     if (rowCount == 0) {
         cout << "No data to display." << endl;
         return;
     }
 
-    for (int i = 0; i < rowCount; ++i) {
-        cout << data[i].id << " " << data[i].name << " " << data[i].game << " " << data[i].score << endl;
+    for ( i = 0; i < rowCount; ++i) {
+        cout << data[i].id << " " 
+            << data[i].name << " " 
+            << data[i].game << " " 
+            << data[i].score << endl;
     }
     displayCount++;
+    cout << "--------------";
+
 }
 
 int search(const string& chosenId) {
-    for (int i = 0; i < rowCount; i++) {
+    for ( i = 0; i < rowCount; i++) {
         if (data[i].id == chosenId) {
             return i; // Return the index of the found gamer
         }
     }
     return -1; // ID not found
+}
+
+void sortById(){
+    for ( i = 0; i < rowCount - 1; ++i) {
+        for (int j = 0; j < rowCount - i - 1; ++j) {
+            if (data[j].id > data[j + 1].id){
+                // Swap data[j] and data[j+1]
+                info temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+   saveToFile();
+}
+
+void saveToFile() {
+    ofstream outFile("gameData.txt");
+    if (!outFile.is_open()) {
+        cout << "Error opening file for writing." << endl;
+        return;
+    }
+    for ( i = 0; i < rowCount; ++i) {
+        outFile 
+            << data[i].id << endl 
+            << data[i].name << endl 
+            << data[i].game << endl 
+            << data[i].score << endl;
+    }
+    cout << "Data saved to gameData.txt successfully!" << endl;
+    outFile.close();
+}
+
+void updateScore(string idToUpdate,string newScore) {
+    int index = search(idToUpdate);
+    if (index != -1) {
+        data[index].score = newScore;
+        cout << "Score updated successfully!" << endl;
+    } else {
+        cout << "ID not found. Score not updated." << endl;
+    }
+        saveToFile();
+    Display();
 }
